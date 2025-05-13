49fcdb389e9415f5c9d4a6d5d5cbd0a015b25400
JoelAAs <joel.as.0808@gmail.com>
2025-05-13 17:35:39 +0200

    reformating

--- src/FormatFiltering/FormatingFiltering.smk
+++ src/FormatFiltering/FormatingFiltering.smk
@@ -1,21 +1,17 @@
-import gzip
-from fileinput import input
-
-import pandas as pd
-import numpy as np
 from formating import *
 
-
-
 ##### Rules
-rule format_intact:
+rule format_miTab:
+    """
+    Filter and format miTab interaction file into bait-prey-publication-detection_method csv
+    """
     input:
-        intact = "data/intact/human.txt"
+        miTab = "data/intact/human.txt"
     output:
-        formated = "work_folder/intact/method_subset/bait_prey_publications.csv"
+        formated = "work_folder/formated/bait_prey_publications.csv"
     run:
-        intact_df = read_intact(input.intact)
-        bait_prey_df = filter_and_index(intact_df)
+        intact_df = filter_mitab(input.miTab)
+        bait_prey_df = reform_to_bait_prey(intact_df)
         bait_prey_df.to_csv(
             output.formated,
             sep="\t",
@@ -23,65 +19,12 @@ rule format_intact:
         )
 
 rule get_gene_name_uniprot:
+    """
+    Extract uniprotID-gene_name from miTab aliases column
+    """
     input:
-        intact = "data/intact/human.txt"
+        miTab = "data/intact/human.txt"
     output:
         uniprot = "work_folder/intact/uniprot_to_gene_name.csv"
     run:
-        get_gene_names(input.intact, output.uniprot)
-
-rule subset_method:
-    input:
-        formated = "work_folder/intact/method_subset/bait_prey_publications.csv"
-    output:
-        expand(
-            "work_folder/intact/method_subset/{method}.csv", method=config["methods"]
-        )
-    run:
-        intact_df = pd.read_csv(input.formated, sep="\t")
-        for method in config["methods"]:
-            method_subset = intact_df[intact_df["detection_method"] == method]
-            method_subset.to_csv(
-                f"work_folder/intact/method_subset/{method}.csv",
-                sep="\t",
-                index=False
-            )
-
-
-rule get_ppi_counts:
-    params:
-        max_cl_mentions = 1
-    input:
-        pubmed_id = "work_folder/pid_cell_line/{cell_line}.csv",
-        method_subset = "work_folder/intact/method_subset/{method}.csv",
-        prey_pod = "work_folder/cell_type_pod/{cell_line}_pod.csv"
-    output:
-        protein_pairs = "work_folder/intact/pair_count/ppi_pair_counts_{cell_line}_{method}.csv"
-
-    run:
-        pid_df = pd.read_csv(input.pubmed_id,
-            sep="\t",
-            dtype={"pubmed_id": str, "cl_count": int}
-        )
-
-        #pids = pid_df["pubmed_id"].unique()
-        method_df = pd.read_csv(
-            input.method_subset,
-            sep="\t",
-            dtype="str"
-        )
-        method_cl_df = method_df.merge(pid_df,on="pubmed_id",how="inner")
-        method_cl_df = method_cl_df[method_cl_df["cl_count"] < params.max_cl_mentions]
-        method_type =  config["methods"][wildcards.method]
-
-        max_interaction_dict, observed_interaction_dict  = get_interaction_dict(
-            bait_prey_df=method_cl_df,
-            method=method_type,
-            prey_file=input.prey_pod)
-
-        dict_to_pairs_file(
-            max_interaction_dict=max_interaction_dict,
-            observed_interaction_dict=observed_interaction_dict,
-            output_filename=output.protein_pairs,
-            method = method_type,
-            prey_pod_file = input.prey_pod)
+        get_gene_names(input.miTab, output.uniprot)
--- src/FormatFiltering/formating.py
+++ src/FormatFiltering/formating.py
@@ -2,9 +2,6 @@ import pandas as pd
 import re
 import numpy as np
 
-## TODO: separate and rename files, this isn't all formating
-
-
 def _find_pattern(cell, pattern, single=True):
     match = re.search(pattern, cell)
     if match:
@@ -14,7 +11,81 @@ def _find_pattern(cell, pattern, single=True):
         return match.groups()
     return None
 
+def filter_mitab(filename):
+    """
+    Remove non-human proteins.
+    Remove any non bait-prey interaction.
+    Format publications ids and uniprot identifiers
+    :param filename: miTab interaction file
+    :return: filtered and formated pandas dataframe
+    """
+    mitab_df = pd.read_csv(filename, sep="\t")
+    mitab_df = mitab_df[
+        (
+                mitab_df['Taxid interactor A'].str.contains("9606") &
+                mitab_df['Taxid interactor B'].str.contains("9606")
+        )
+    ]
+    mitab_df['RoleA'] = mitab_df['Experimental role(s) interactor A'].str.contains("bait")
+    mitab_df['RoleB'] = mitab_df['Experimental role(s) interactor B'].str.contains("bait")
+
+    # No bait-bait prey-prey pairs
+    mitab_df = mitab_df[mitab_df['RoleA'] ^ mitab_df['RoleB']]
+
+    mitab_df["pubmed_id"] = mitab_df["Publication Identifier(s)"].apply(
+        _find_pattern, args=(r"pubmed:(\d+)",))
+    mitab_df["IDA"] = mitab_df["#ID(s) interactor A"].apply(
+        _find_pattern, args=(r"uniprotkb:(.+)",))
+    mitab_df["IDB"] = mitab_df["ID(s) interactor B"].apply(
+        _find_pattern, args=(r"uniprotkb:(.+)",))
+    mitab_df["detection_method"] = mitab_df["Interaction detection method(s)"].apply(
+        _find_pattern, args=(r"psi-mi:\"(MI:\d+)\"",))
+
+    return mitab_df
+
+
+def reform_to_bait_prey(mitab_df):
+    """
+    Reform PPI pandas dataframe into bait-prey format
+    :param mitab_df: input filtered
+    :return: data frame with columns:
+        uniprot_bait
+        uniprot_prey
+        pubmed_id
+        detection_method
+    """
+    mitab_df = mitab_df[["IDA", "IDB", "RoleA", "RoleB", "detection_method", "pubmed_id"]].drop_duplicates(keep="first")
+    mitab_df = mitab_df.dropna()
+    mitab_df = mitab_df[mitab_df["RoleA"] ^ mitab_df["RoleB"]]
+    reform_list = []
+    for _, row in mitab_df.iterrows():
+        if row["RoleA"]:
+            bait = row["IDA"]
+            prey = row["IDB"]
+        else:
+            bait = row["IDB"]
+            prey = row["IDA"]
+
+        reform_list.append({
+            "uniprot_bait": bait,
+            "uniprot_prey": prey,
+            "pubmed_id": row["pubmed_id"],
+            "detection_method": row["detection_method"].replace(":", "-")
+        })
+
+    bait_prey_df = pd.DataFrame(reform_list)
+    bait_prey_df = bait_prey_df.drop_duplicates()
+
+    return bait_prey_df
+
+
 def get_gene_names(filename, output_file):
+    """
+    Extracting uniprot-gene name from miTab interaction file
+    :param filename: Mitab file location
+    :param output_file: uniprot - gene_name file location
+    :return:
+    """
     intact_df = pd.read_csv(filename, sep="\t")
     intact_df = intact_df[
         (
@@ -47,151 +118,100 @@ def get_gene_names(filename, output_file):
         for uniprot_id, gene_name in name_dict.items():
             w.write(f"{uniprot_id}\t{gene_name}\n")
 
-def read_intact(filename):
-    intact_df = pd.read_csv(filename, sep="\t")
-    intact_df = intact_df[
-        (
-                intact_df['Taxid interactor A'].str.contains("9606") &
-                intact_df['Taxid interactor B'].str.contains("9606")
-        )
-    ]
-
-    intact_df['RoleA'] = intact_df['Experimental role(s) interactor A'].str.contains("bait")
-    intact_df['RoleB'] = intact_df['Experimental role(s) interactor B'].str.contains("bait")
-    # No bait-bait prey-prey pairs
-    intact_df = intact_df[intact_df['RoleA'] ^ intact_df['RoleB']]
 
 
-    intact_df["pubmedID"] = intact_df["Publication Identifier(s)"].apply(
-        _find_pattern, args=(r"pubmed:(\d+)",))
-    intact_df["IDA"] = intact_df["#ID(s) interactor A"].apply(
-        _find_pattern, args=(r"uniprotkb:(.+)",))
-    intact_df["IDB"] = intact_df["ID(s) interactor B"].apply(
-        _find_pattern, args=(r"uniprotkb:(.+)",))
-    intact_df["detectionMethod"] = intact_df["Interaction detection method(s)"].apply(
-        _find_pattern, args=(r"psi-mi:\"(MI:\d+)\"",))
 
-
-    return intact_df
-
-def filter_and_index(intact_df):
-    intact_df = intact_df[["IDA", "IDB", "RoleA", "RoleB", "detectionMethod", "pubmedID"]].drop_duplicates(keep="first")
-    intact_df = intact_df.dropna()
-    intact_df = intact_df[intact_df["RoleA"] ^ intact_df["RoleB"]]
-    reform_list = []
-    for _, row in intact_df.iterrows():
-        if row["RoleA"]:
-            bait = row["IDA"]
-            prey = row["IDB"]
-        else:
-            bait = row["IDB"]
-            prey = row["IDA"]
-
-        reform_list.append({
-            "bait": bait,
-            "prey": prey,
-            "pubmed_id": row["pubmedID"],
-            "detection_method": row["detectionMethod"].replace(":", "-")
-        })
-
-    bait_prey_df = pd.DataFrame(reform_list)
-    bait_prey_df = bait_prey_df.drop_duplicates()
-
-    return bait_prey_df
-
-def create_or_update(current_dict, bait, prey):
-    if bait in current_dict:
-        if prey in current_dict[bait]:
-            current_dict[bait][prey] += 1
-        else:
-            current_dict[bait][prey] = 1
-    else:
-        current_dict[bait] = {prey: 1}
-
-    return current_dict
-
-
-def get_interaction_dict(bait_prey_df, method="Y2H-pooling", prey_file=None):
-    cell_line_preys = []
-    if method == "MS":
-        prey_df = pd.read_csv(prey_file, sep="\t")
-        cell_line_preys = prey_df["uniprot_id"].tolist()
-    elif method != "Y2H-pooling":
-        raise ValueError(f"The method type: {method} is has no prey-bait matching strategy")
-
-
-    max_interaction_dict = dict()
-    observed_interaction_dict = dict()
-
-    studies = bait_prey_df["pubmed_id"].unique()
-    for study in studies:
-        study_df = bait_prey_df[bait_prey_df["pubmed_id"] == study]
-        baits = set()
-        preys = set()
-        for _, row in study_df.iterrows():
-            bait = row["bait"]
-            prey = row["prey"]
-
-            baits.update({bait})
-            preys.update({prey})
-
-            observed_interaction_dict = create_or_update(
-                observed_interaction_dict,
-                bait=bait,
-                prey=prey
-            )
-
-            if method == "MS" and prey not in cell_line_preys:
-                # Identified prey without POD estimate
-                max_interaction_dict = create_or_update(
-                    max_interaction_dict,
-                    bait=bait,
-                    prey=prey
-                )
-        if method == "MS":
-            # Observe we assume that test all baits as separate MS runs.
-            # This should be the standard way of doing things.
-            preys = cell_line_preys
-
-        for bait in baits:
-            for prey in preys:
-                max_interaction_dict = create_or_update(
-                    max_interaction_dict,
-                    bait=bait,
-                    prey=prey
-                )
-
-    return max_interaction_dict, observed_interaction_dict
-
-def dict_to_pairs_file(max_interaction_dict, observed_interaction_dict, output_filename, method = "Y2H-pooling", prey_pod_file =None):
-    pod_dict = dict()
-    if method == "MS":
-        with open(prey_pod_file, "r") as f:
-            for l in f:
-                gene_name, protein_id, prey_pod = l.strip().split("\t")
-                pod_dict[protein_id] = prey_pod
-
-
-    with open(output_filename, "w") as w:
-        w.write("prey\tbait\tmax_interactions\tobserved_interactions\tprey_pod\n")
-        for bait in max_interaction_dict:
-            for prey in max_interaction_dict[bait]:
-                if method == "MS":
-                    if prey in pod_dict:
-                        prey_pod = pod_dict[prey]
-                    else:
-                        prey_pod = None
-                else:
-                    prey_pod = 1
-
-                obs_count = 0
-                if prey in observed_interaction_dict[bait]:
-                    obs_count = observed_interaction_dict[bait][prey]
-
-                w.write(f"{bait}\t"
-                        f"{prey}\t"
-                        f"{max_interaction_dict[bait][prey]}\t"
-                        f"{obs_count}\t"
-                        f"{prey_pod}\n")
-
-
+# def create_or_update(current_dict, bait, prey):
+#     if bait in current_dict:
+#         if prey in current_dict[bait]:
+#             current_dict[bait][prey] += 1
+#         else:
+#             current_dict[bait][prey] = 1
+#     else:
+#         current_dict[bait] = {prey: 1}
+#
+#     return current_dict
+#
+#
+# def get_interaction_dict(bait_prey_df, method="Y2H-pooling", prey_file=None):
+#     cell_line_preys = []
+#     if method == "MS":
+#         prey_df = pd.read_csv(prey_file, sep="\t")
+#         cell_line_preys = prey_df["uniprot_id"].tolist()
+#     elif method != "Y2H-pooling":
+#         raise ValueError(f"The method type: {method} is has no prey-bait matching strategy")
+#
+#     max_interaction_dict = dict()
+#     observed_interaction_dict = dict()
+#
+#     studies = bait_prey_df["pubmed_id"].unique()
+#     for study in studies:
+#         study_df = bait_prey_df[bait_prey_df["pubmed_id"] == study]
+#         baits = set()
+#         preys = set()
+#         for _, row in study_df.iterrows():
+#             bait = row["bait"]
+#             prey = row["prey"]
+#
+#             baits.update({bait})
+#             preys.update({prey})
+#
+#             observed_interaction_dict = create_or_update(
+#                 observed_interaction_dict,
+#                 bait=bait,
+#                 prey=prey
+#             )
+#
+#             if method == "MS" and prey not in cell_line_preys:
+#                 # Identified prey without POD estimate
+#                 max_interaction_dict = create_or_update(
+#                     max_interaction_dict,
+#                     bait=bait,
+#                     prey=prey
+#                 )
+#         if method == "MS":
+#             # Observe we assume that test all baits as separate MS runs.
+#             # This should be the standard way of doing things.
+#             preys = cell_line_preys
+#
+#         for bait in baits:
+#             for prey in preys:
+#                 max_interaction_dict = create_or_update(
+#                     max_interaction_dict,
+#                     bait=bait,
+#                     prey=prey
+#                 )
+#
+#     return max_interaction_dict, observed_interaction_dict
+#
+#
+# def dict_to_pairs_file(max_interaction_dict, observed_interaction_dict, output_filename, method="Y2H-pooling",
+#                        prey_pod_file=None):
+#     pod_dict = dict()
+#     if method == "MS":
+#         with open(prey_pod_file, "r") as f:
+#             for l in f:
+#                 gene_name, protein_id, prey_pod = l.strip().split("\t")
+#                 pod_dict[protein_id] = prey_pod
+#
+#     with open(output_filename, "w") as w:
+#         w.write("prey\tbait\tmax_interactions\tobserved_interactions\tprey_pod\n")
+#         for bait in max_interaction_dict:
+#             for prey in max_interaction_dict[bait]:
+#                 if method == "MS":
+#                     if prey in pod_dict:
+#                         prey_pod = pod_dict[prey]
+#                     else:
+#                         prey_pod = None
+#                 else:
+#                     prey_pod = 1
+#
+#                 obs_count = 0
+#                 if prey in observed_interaction_dict[bait]:
+#                     obs_count = observed_interaction_dict[bait][prey]
+#
+#                 w.write(f"{bait}\t"
+#                         f"{prey}\t"
+#                         f"{max_interaction_dict[bait][prey]}\t"
+#                         f"{obs_count}\t"
+#                         f"{prey_pod}\n")
