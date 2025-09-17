0e0dfa20ec08e39f71c6e635ff30c492c99efd86
JoelAAs <joel.as.0808@gmail.com>
2025-09-17 13:24:21 +0200

    datatype missmatch

--- src/Analysis/Annotation/mean_distance_support.py
+++ src/Analysis/Annotation/mean_distance_support.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from pyarrow import float64
+
 
 def get_cumulative_sum(df, value_column, cumulative_columns, greater=True, min_samples=200):
     bins = df[value_column].unique()
@@ -20,8 +22,8 @@ def get_cumulative_sum(df, value_column, cumulative_columns, greater=True, min_s
     measurement_matrix = df[cumulative_columns].iloc[idx_val]
     na_matrix = measurement_matrix.isna().to_numpy()
     measurement_matrix = measurement_matrix.to_numpy()
-    cumulative_sum = np.zeros(len(cumulative_columns))
-    cumulative_na = np.zeros(len(cumulative_columns))
+    cumulative_sum = np.zeros(len(cumulative_columns),dtype=np.float64)
+    cumulative_na = np.zeros(len(cumulative_columns),dtype=int)
 
     previous = 0
     i = 0
@@ -31,16 +33,20 @@ def get_cumulative_sum(df, value_column, cumulative_columns, greater=True, min_s
         while (i < len(values) and threshold <= values[i]) or i < min_samples:
             i += 1
         if previous != i:
-            cumulative_sum += np.nansum(measurement_matrix[previous:i], axis=0)
-            cumulative_na += np.nansum(na_matrix[previous:i], axis=0)
+            cumulative_sum += np.nansum(measurement_matrix[previous:i], axis=0, dtype=np.float64)
+            cumulative_na += np.nansum(na_matrix[previous:i], axis=0, dtype=int)
             previous = i
             rows[j] = {
                 "limit": value_column,
-                "value": (threshold if greater else -threshold),
-            } + {
-                f"sum_{c}": v for c,v in zip(cumulative_sum, cumulative_sum)
-            } + {
-                f"non_na_pairs_{c}": v for c, v in zip(cumulative_na, cumulative_sum)
+                **{
+                    "value": (threshold if greater else -threshold)
+                 },
+                **{
+                    f"sum_{c}": v for c,v in zip(cumulative_columns, cumulative_sum)
+                },
+                **{
+                    f"non_na_pairs_{c}": i-v for c, v in zip(cumulative_columns, cumulative_na)
+                }
             }
             j += 1
     return pd.DataFrame(rows).dropna()
