7956e569bd87dd9e4d4c850bd6e66381fa1a4f1c
Joel <joel.as.0808@gmail.com>
2025-11-26 16:29:06 +0100

    for isoform statistics

--- scripts/check_isofroms.py
+++ scripts/check_isofroms.py
@@ -10,16 +10,16 @@ df_ms = df_ms.merge(gene_name_df, left_on="uniprot_id_prey", right_on="uniprot_i
                                   suffixes=("_bait", "_prey"))
 del df_ms["uniprot_id"]
 
-n_ids_baits = df_ms.groupby("gene_name_bait", as_index=False).nunique("uniprot_id_bait")
-n_ids_prey = df_ms.groupby("gene_name_prey", as_index=False).nunique("uniprot_id_prey")
+n_ids_baits = df_ms.groupby("gene_name_bait", as_index=False)["uniprot_id_bait"].nunique()
+n_ids_prey = df_ms.groupby("gene_name_prey", as_index=False)["uniprot_id_prey"].nunique()
 
 print(f'The number of baits with more than 1 uniprot identifiers per gene:')
-print(f'{sum(n_ids_baits["size"] != 1)} / {sum(n_ids_baits["size"] != 1)}')
+print(f'{sum(n_ids_baits["uniprot_id_bait"] != 1)} / {sum(n_ids_baits["uniprot_id_bait"] != 1)}')
 
 print(f'The number of prey with more than 1 uniprot identifiers per gene:')
-print(f'{sum(n_ids_prey["size"] != 1)} / {sum(n_ids_prey["size"] != 1)}')
+print(f'{sum(n_ids_prey["uniprot_id_prey"] != 1)} / {sum(n_ids_prey["uniprot_id_prey"] != 1)}')
 
-bait_multi_ids = n_ids_baits[n_ids_baits["size"] != 1, "gene_name_bait"]
+bait_multi_ids = n_ids_baits[n_ids_baits["uniprot_id_bait"] != 1, "gene_name_bait"]
 df_ms_multi_id = df_ms[df_ms["gene_name_bait"].isin(bait_multi_ids)]
 
 def number_of_isoforms(id_list):
