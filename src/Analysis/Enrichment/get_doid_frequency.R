317b0121c10a2403018e7354c74e4c6b60da9396
JoelAAs <joel.as.0808@gmail.com>
2025-12-11 18:24:54 +0100

    slice_max, implicit behaviour

--- src/Analysis/Enrichment/get_doid_frequency.R
+++ src/Analysis/Enrichment/get_doid_frequency.R
@@ -8,7 +8,7 @@ args <- commandArgs(trailingOnly = TRUE)
 input_degree  <- args[1]
 n_top         <- as.numeric(args[2])
 val_column        <- args[3]
-doid_count_file <- args[5]
+doid_count_file <- args[4]
 doid_annotation_file <- args[5]
 
 
@@ -26,10 +26,11 @@ entrez_id <- mapIds(
 
 gene_df = as.data.frame(entrez_id)
 gene_df$gene_name <- row.names(gene_df)
-degree_df <- merge(degree_df, gene_df, on="entrez_id", all=TRUE)
+degree_df <- merge(degree_df, gene_df, on="entrez_id",left_all=TRUE)
 degree_df %>%
-    slice_max(!!sym(val_column), n=n_top) -> degree_df
+    slice_max(!!sym(val_column), n=n_top,with_ties = FALSE) -> degree_df
 degree_df %>% pull(entrez_id) -> genes
+
 
 
 doids <- select(
@@ -38,13 +39,13 @@ doids <- select(
   columns = c("doid"))
 
 colnames(doids) <- c("doid", "entrez_id")
-degree_df <- merge(doids, degree_df, on="entrez_id", all=TRUE)
-degree_df %>%
+degree_df_doid <- merge(degree_df, doids, on="entrez_id", left_all=TRUE)
+degree_df_doid %>%
     group_by(doid) %>%
     summarize(
         doid_frequency = n()/n_top
     ) -> doid_count
 
 write.table(doid_count,doid_count_file,sep="\t", row.names = FALSE)
-write.table(doid_count,doid_annotation_file,sep="\t", row.names = FALSE)
+write.table(degree_df_doid,doid_annotation_file,sep="\t", row.names = FALSE)
 
