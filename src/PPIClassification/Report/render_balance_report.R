b30b478d9150068eaf9d52e2cc34bb0de5fd0e73
JoelAAs <joel.as.0808@gmail.com>
2026-02-10 18:30:35 +0100

    rmd and snakemake-fs problems

---
+++ src/PPIClassification/Report/render_balance_report.R
@@ -0,0 +1,22 @@
+rmarkdown::render(
+  snakemake@params$rmd_location,
+  output_file = basename(snakemake@output$html_report),
+  output_dir = dirname(snakemake@output$html_report),
+  params = list(
+    full_pos_file = snakemake@input$full_pos,
+    full_neg_file = snakemake@input$full_neg,
+    full_neg_file = snakemake@input$full_neg,
+    train_pos_file = snakemake@input$train_pos,
+    train_neg_file = snakemake@input$train_neg,
+    train_maxflow_pos_file = snakemake@input$train_maxflow_pos,
+    train_maxflow_neg_file = snakemake@input$train_maxflow_neg,
+    validate_pos_file = snakemake@input$validate_pos,
+    validate_neg_file = snakemake@input$validate_neg,
+    validate_maxflow_pos_file = snakemake@input$validate_maxflow_pos,
+    validate_maxflow_neg_file = snakemake@input$validate_maxflow_neg,
+    test_pos_file = snakemake@input$test_pos,
+    test_neg_file = snakemake@input$test_neg,
+    test_maxflow_pos_file = snakemake@input$test_maxflow_pos,
+    test_maxflow_neg_file = snakemake@input$test_maxflow_neg
+  )
+)
