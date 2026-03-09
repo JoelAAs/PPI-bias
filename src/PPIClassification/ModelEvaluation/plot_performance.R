3fd648de9442dce3d62ca0b78094ff1a7e4b9de7
JoelAAs <joel.as.0808@gmail.com>
2026-03-09 19:44:31 +0100

    misc report plots

--- src/PPIClassification/ModelEvaluation/plot_performance.R
+++ src/PPIClassification/ModelEvaluation/plot_performance.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(ggbeeswarm)
 library(tidyverse)
 
 auc_data <- read.csv(
@@ -150,32 +151,41 @@ df_balance_undir <- df_balance_undir %>%
     TRUE ~ NA_character_
   ))
 
-df_balance_undir <- df_balance_undir %>% filter(partition == "sequencesimilarity")
-
-
 g <- ggplot(
   df_balance_undir,
   aes(
-    y = p_undir,
-    x = sp_undir
+    y = sp_undir,
+    x = partition
   )
 ) +
-  geom_point(aes(color = random, shape = set_type)) +
+  geom_beeswarm(aes(color = random, shape = set_type), cex = 3, priority = "density", size = 1) +
   labs(
-    title = "Degree balance score between HCNI and non-observed\nUndirected",
-    x = "Spearman correlation",
-    y = "Pearson correlation",
-    color = "Negative data"
+    title = "Correlation of Positive and negative degree:\nInteraction network",
+    x = "Gene partition",
+    y = "Spearman correlation",
+    color = "Negative data",
+    shape = "Set"
   ) +
   scale_color_manual(
     values = c("darkorange", "blue"),
     labels = c("HCNI", "Non-observed")
   ) +
-  xlim(0, 1) +
   ylim(0, 1) +
   theme_bw() +
-  facet_grid(selection ~ dataset) +
-  # scale_x_discrete(labels = function(x) sub("\\..*", "", x)) +
+  facet_grid(selection ~ dataset, labeller = labeller(
+    dataset = c(
+      flat = "Combined",
+      ms = "MS",
+      y2h = "Y2H"
+    )
+  )) +
+  scale_x_discrete(labels = function(x) {
+    case_when(
+      str_remove(x, "\\..*") == "maxpos" ~ "MaxPos",
+      str_remove(x, "\\..*") == "sequencesimilarity" ~ "SeqSim",
+      TRUE ~ x
+    )
+  }) +
   theme(
     legend.position = "right",
     axis.text.x = element_text(angle = -45, hjust = 0, vjust = 0)
@@ -211,7 +221,6 @@ df_balance_dir <- df_balance_dir %>%
     TRUE ~ NA_character_
   ))
 
-df_balance_dir <- df_balance_dir %>% filter(partition == "sequencesimilarity")
 
 g <- ggplot(
   df_balance_dir,
@@ -220,22 +229,32 @@ g <- ggplot(
     x = sp_prey
   )
 ) +
-  geom_point(aes(color = random, shape = set_type)) +
+  geom_point(aes(color = interaction(random, partition), shape = set_type)) +
   labs(
-    title = "Degree balance score between HCNI and non-observed\nDirected",
+    title = "Correlation of Positive and negative degree:\nBait-prey network",
     x = "Spearman correlation (prey)",
     y = "Spearman correlation (bait)",
-    color = "Negative data"
+    color = "Negative data",
+    shape = "Set"
   ) +
   scale_color_manual(
-    values = c("darkorange", "blue"),
-    labels = c("HCNI", "Non-observed")
+    values = c("darkorange", "blue", "#BA3F1D", "#80A1C1"),
+    labels = c(
+      "HCNI:MaxPos", "Non-observed:MaxPos",
+      "HCNI:SeqSim", "Non-observed:SeqSim"
+    )
   ) +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
   xlim(0, 1) +
   ylim(0, 1) +
   theme_bw() +
-  facet_grid(selection ~ dataset) +
-  # scale_x_discrete(labels = function(x) sub("\\..*", "", x)) +
+  facet_grid(selection ~ dataset, labeller = labeller(
+    dataset = c(
+      flat = "Combined",
+      ms = "MS",
+      y2h = "Y2H"
+    )
+  )) +
   theme(
     legend.position = "right",
     axis.text.x = element_text(angle = -45, hjust = 0, vjust = 0)
@@ -243,52 +262,86 @@ g <- ggplot(
 
 ggsave("degree_balance_dir.png", g, height = 4, width = 6)
 
-g <- ggplot(
-  df_balance_dir,
-  aes(
-    y = ws_bait,
-    x = ws_prey
-  )
-) +
-  geom_point(aes(color = random, shape = set_type)) +
-  labs(
-    title = "Degree balance score between HCNI and non-observed\nDirected",
-    x = "Wasserstein metric (prey)",
-    y = "Wasserstein metric (bait)",
-    color = "Negative data"
+df_edge_count <- read.table(
+  "edge_count.csv",
+  header = F, col.names = c("model", "n_edges")
+)
+
+df_edge_count$dataset <- sapply(
+  df_edge_count$model, function(x) strsplit(x, "_")[[1]][1]
+)
+df_edge_count$positive_lim <- sapply(
+  df_edge_count$model, function(x) strsplit(x, "_")[[1]][4]
+)
+
+df_edge_count$network_type <- sapply(
+  df_edge_count$model, function(x) strsplit(x, "_")[[1]][2]
+)
+
+df_edge_count_dir <- df_edge_count %>%
+  filter(network_type == "directional")
+
+df_joined <- df_balance_dir %>%
+  filter(random == FALSE) %>%
+  left_join(df_edge_count_dir, by = c("dataset", "positive_lim"))
+
+
+df_joined[, "percent_retained"] <- 100 * df_joined$n_pos / df_joined$n_edges
+df_all <- df_joined %>%
+  group_by(dataset, partition, selection, positive_lim) %>%
+  summarise(
+    n_pos = sum(n_pos),
+    .groups = "drop"
+  ) %>%
+  mutate(
+    set_type = "total",
+  ) %>%
+  left_join(df_edge_count_dir, by = c("dataset", "positive_lim"))
+
+df_all[, "percent_retained"] <- 100 * df_all$n_pos / df_all$n_edges
+
+df_plot <- bind_rows(df_joined, df_all)
+df_plot$set_type <- factor(df_plot$set_type, levels = c("total", "train", "val", "test"))
+
+g <- ggplot(df_plot, aes(x = partition, y = selection)) +
+  geom_tile(
+    aes(
+      fill = percent_retained
+    )
   ) +
-  scale_color_manual(
-    values = c("darkorange", "blue"),
-    labels = c("HCNI", "Non-observed")
+  geom_text(
+    aes(
+      label = sprintf("%.1f", percent_retained)
+    ),
+    color = "white", size = 3
+  ) +
+  scale_fill_gradient(high = "darkorange", low = "blue") +
+  labs(
+    title = "Positive edges retained in after partitioning",
+    x = "Gene partition", y = "Selection", fill = " % edges retained"
   ) +
   theme_bw() +
-  facet_grid(selection ~ dataset) +
-  # scale_x_discrete(labels = function(x) sub("\\..*", "", x)) +
-  theme(
-    legend.position = "right",
-    axis.text.x = element_text(angle = -45, hjust = 0, vjust = 0)
-  )
+  theme(axis.text.x = element_text(angle = -45), legend.position = "right") +
+  facet_wrap(~ dataset + set_type, labeller = labeller(
+    dataset = c(
+      flat = "Combined",
+      ms = "MS",
+      y2h = "Y2H"
+    ),
+    set_type = c(
+      total = "Total",
+      train = "Train",
+      val = "Validation",
+      test = "Test"
+    )
+  )) +
+  scale_x_discrete(labels = function(x) {
+    case_when(
+      str_remove(x, "\\..*") == "maxpos" ~ "MaxPos",
+      str_remove(x, "\\..*") == "sequencesimilarity" ~ "SeqSim",
+      TRUE ~ x
+    )
+  })
 
-ggsave("degree_ws_dir.png", g, height = 4, width = 6)
 
-
-
-df <- read.table("prediction_dist_ms_loose_directional_ss.csv", header = T)
-
-g <- ggplot(df,
-  aes(x =value, fill = negative_data)
-  ) +
-  geom_density(alpha = 0.5) +
-  theme_bw() +
-  labs(
-    title = "Prediction distribution for MS loose directional sequence similarity",
-    x = "Predicted probability",
-    y = "Density",
-    fill = "Negative data"
-  ) +
-  theme(
-    legend.position = "right"
-  )
-
-ggsave("prob_denstity.png", g, height = 4, width = 6)
-
+ggsave("percent_retained.png", g, height = 6, width = 6)
