1b236332336453120a9bc600d90014389220cc99
JoelAAs <joel.as.0808@gmail.com>
2026-03-27 14:50:28 +0100

    wrong input name to script

--- src/PPIClassification/DataSplit/random_negative.py
+++ src/PPIClassification/DataSplit/random_negative.py
@@ -1,10 +1,9 @@
 import networkx as nx
 from degree_balaning_functions import back_and_forth_max_flow
-np.random.seed(1234)
 
 
 def main():
-    pos_df = pd.read_csv(snakemake.input.set_pos, sep="\t", header=None)
+    pos_df = pd.read_csv(snakemake.input.balanced_pos, sep="\t", header=None)
     G_pos = nx.from_pandas_edgelist(pos_df, 0, 1, create_using=nx.DiGraph)
 
     G_comp = nx.DiGraph()
@@ -17,7 +16,7 @@ def main():
     
     selected_G, _ = back_and_forth_max_flow(G_pos, G_comp)
     nx.write_edgelist(
-        selected_G[1], snakemake.output.train_neg, delimiter="\t", data=False
+        selected_G[1], snakemake.output.set_random_neg, delimiter="\t", data=False
     )
 
 main()
